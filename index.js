7df7fa42a2326907d31f2d45267ce46412945a31
GutoCoimbra <gutembergcoimbra@hotmail.com>
2024-03-09 14:49:09 -0300

    add feature 3

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   console.log("New feature");
 });
+app.get("/", (req, res) => {
+  console.log("New feature3");
+});
 
 app.listen(port, () => {
   console.log(`start listening ${port}`);
